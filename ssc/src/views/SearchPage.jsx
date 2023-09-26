e576b866de4868a8fee27e93383390ff8fab1753
Stanagley <stanleyguo333@gmail.com>
2023-09-26 17:29:04 -0400

    updated search capabilities to include filtering

--- ssc/src/views/SearchPage.jsx
+++ ssc/src/views/SearchPage.jsx
@@ -11,37 +11,77 @@ function SearchPage() {
     };
 
     const [searchResults, setSearchResults] = useState([]); // State to store search results
+    const [searchBy, setSearchBy] = useState('major'); // Default search criteria is 'major'
 
     const handleSearch = (query) => {
         // Fetch data from the JSON file
         fetch('http://localhost:9000/users')
             .then((response) => response.json())
             .then((data) => {
-                // Implement a search algorithm to filter the results based on the major property
+                // Implement a search algorithm to filter the results based on the selected criteria
                 const filteredResults = data.filter((user) => {
                     // You can customize this search logic as needed
-                    return user.major.toLowerCase().includes(query.toLowerCase());
+                    if (searchBy === 'major') {
+                        return user.major.toLowerCase().includes(query.toLowerCase());
+                    } else if (searchBy === 'location') {
+                        return user.location.toLowerCase().includes(query.toLowerCase());
+                    }
+                    else if (searchBy === 'class') {
+                        return user.class.toLowerCase().includes(query.toLowerCase());
+                    }
+                    else if (searchBy === 'time') {
+                        return user.time.toLowerCase().includes(query.toLowerCase());
+                    }
                 });
-
-                // Update the state with the filtered results or display "No results found"
-                setSearchResults(filteredResults.length > 0 ? filteredResults : []);
+    
+                // Update the state with the filtered results
+                setSearchResults(filteredResults);
             })
             .catch((error) => {
                 console.error('Error fetching data:', error);
             });
     };
 
+    const handleSearchCriteriaChange = (event) => {
+        setSearchBy(event.target.value);
+    };
+
     return (
         <div style={styles.container}>
             <h2 style={styles.title}>Search Page</h2>
-            <p style={styles.description}>Enter the major you want to search for:</p>
+            <p style={styles.description}>Select search criteria:</p>
+            <div>
+                <label>
+                    <input type="radio" value="major" checked={searchBy === 'major'} onChange={handleSearchCriteriaChange} />
+                    Major
+                </label>
+                <label>
+                    <input type="radio" value="location" checked={searchBy === 'location'} onChange={handleSearchCriteriaChange} />
+                    Location
+                </label>
+                <label>
+                    <input type="radio" value="class" checked={searchBy === 'class'} onChange={handleSearchCriteriaChange} />
+                    Class
+                </label>
+                <label>
+                    <input type="radio" value="time" checked={searchBy === 'time'} onChange={handleSearchCriteriaChange} />
+                    Time
+                </label>
+            </div>
+            <p style={styles.description}>Enter your search query below:</p>
             <SearchBar onSearch={handleSearch} />
 
             <div style={styles.searchResults}>
                 {searchResults.length > 0 ? (
                     <ul>
-                        {searchResults.map((result, index) => (
-                            <li key={index}>{result.major}</li>
+                        {searchResults.map((result) => (
+                            <li key={result.id}>
+                                <p>ID: {result.id}</p>
+                                <p>Location: {result.location}</p>
+                                <p>Major: {result.major}</p>
+                                <p>Class: {result.class}</p>
+                                <p>Time: {result.time}</p>
+                            </li>
                         ))}
                     </ul>
                 ) : (
