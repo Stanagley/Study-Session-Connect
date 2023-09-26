d2cf4e536caf23b76ead0d967146ff211ad53482
Yuerden <98784955+Yuerden@users.noreply.github.com>
2023-09-26 16:30:01 -0400

    prefilled sessions for search testing

--- ssc-backend/routes/sessions.js
+++ ssc-backend/routes/sessions.js
@@ -15,12 +15,16 @@ router.get('/', function(req, res, next) {
   res.set('Access-Control-Allow-Origin', '*');
   res.json([{
     id: 1,
-    name: "Hiccup",
-    password: 'hiccup'
+    location: "heaven",
+    major: 'geology',
+    class: 'geo101',
+    time: 'never'
   }, {
     id: 2,
-    name: "King Arthur",
-    password: 'king-arthur'
+    location: "hell",
+    major: 'cs',
+    class: 'rcass.in.my.mouth',
+    time: 'always'
   }]);
   // res.send('respond with a resource');
   
