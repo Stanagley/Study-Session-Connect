f3f4083d78802b336826aff696de1c7547f320d2
Stanagley <stanleyguo333@gmail.com>
2023-10-10 17:14:25 -0400

    rewrote search function to work with backend database

--- ssc-backend/routes/search.js
+++ ssc-backend/routes/search.js
@@ -1,39 +1,41 @@
 const express = require('express');
-const fetch = require('node-fetch');
+const { Pool } = require('pg');
 const router = express.Router();
+
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'ssc',
+  password: 'postgres',
+  port: 5432,
+});
 
 // Define a route for handling searches
 router.get('/', async (req, res) => {
-    const query = req.query.query; // Get the search query from the request query parameters
-    const searchBy = req.query.searchBy; // Get the search criteria from the request query parameters
+  const query = req.query.query; // Get the search query from the request query parameters
+  const searchBy = req.query.searchBy; // Get the search criteria from the request query parameters
 
-    try {
-        // Fetch data from the JSON file or your data source
-        const response = await fetch('http://localhost:9000/');
-        const data = await response.json();
+  try {
+    // Define the valid search criteria based on the available columns
+    const validSearchCriteria = ['id', 'location', 'major', 'course', 'time', 'participants'];
 
-        // Implement your search logic here
-        const filteredResults = data.filter((user) => {
-            if (user && typeof user === 'object') {
-                if (searchBy === 'major' && user.major && typeof user.major === 'string' && user.major.toLowerCase().includes(query.toLowerCase())) {
-                    return true;
-                } else if (searchBy === 'location' && user.location && typeof user.location === 'string' && user.location.toLowerCase().includes(query.toLowerCase())) {
-                    return true;
-                } else if (searchBy === 'class' && user.class && typeof user.class === 'string' && user.class.toLowerCase().includes(query.toLowerCase())) {
-                    return true;
-                } else if (searchBy === 'time' && user.time && typeof user.time === 'string' && user.time.toLowerCase().includes(query.toLowerCase())) {
-                    return true;
-                }
-            }
-            return false;
-        });
-
-        // Send the filtered results as JSON response
-        res.json(filteredResults);
-    } catch (error) {
-        console.error('Error fetching data:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+    if (!validSearchCriteria.includes(searchBy)) {
+      return res.status(400).json({ error: 'Invalid search criteria' });
     }
+
+    // Create a SQL query based on the search criteria and query
+    const sql = `SELECT * FROM sessions WHERE ${searchBy} ILIKE $1`;
+    
+    // Execute the SQL query using the connection pool
+    const { rows } = await pool.query(sql, [`%${query}%`]);
+
+    // Send the results as JSON response
+    res.json(rows);
+  } catch (error) {
+    console.error('Error executing the query:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 module.exports = router;
+
